d4a7b4ea1d0fb1c4dcfabc5d959e726aec08af81
Ninjas4744 <ninjas4744@gmail.com>
2025-09-04 23:39:38 +0300

    Fixed pigeon gyro

--- swerve/gyro/GyroIOPigeon2.java
+++ swerve/gyro/GyroIOPigeon2.java
@@ -1,5 +1,6 @@
 package frc.lib.NinjasLib.swerve.gyro;
 
+import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.hardware.Pigeon2;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -12,37 +13,42 @@ import java.util.Queue;
 public class GyroIOPigeon2 implements GyroIO{
     private Pigeon2 pigeon;
     private StatusSignal<Angle> yaw;
-    private final Queue<Double> yawPositionQueue;
-    private final Queue<Double> yawTimestampQueue;
+    private Queue<Double> yawPositionQueue;
+    private Queue<Double> yawTimestampQueue;
     private boolean inverted;
 
     public GyroIOPigeon2(int id, int frequency, boolean inverted) {
         pigeon = new Pigeon2(id);
         yaw = pigeon.getYaw();
-        yaw.setUpdateFrequency(frequency);
-        pigeon.optimizeBusUtilization();
-        yawTimestampQueue = OdometryThread.getInstance().makeTimestampQueue();
-        yawPositionQueue = OdometryThread.getInstance().registerSignal(yaw.clone());
+        if (frequency > 50) {
+            yaw.setUpdateFrequency(frequency);
+            pigeon.optimizeBusUtilization();
+            yawTimestampQueue = OdometryThread.getInstance().makeTimestampQueue();
+            yawPositionQueue = OdometryThread.getInstance().registerSignal(yaw.clone());
+        }
         this.inverted = inverted;
     }
 
     @Override
     public void updateInputs(GyroIOInputsAutoLogged inputs) {
+        BaseStatusSignal.refreshAll(yaw);
         inputs.Yaw = Rotation2d.fromRadians((inverted ? -1 : 1) * yaw.getValue().in(Units.Radians));
         inputs.Pitch = Rotation2d.fromRadians(pigeon.getPitch().getValue().in(Units.Radians));
         inputs.Roll = Rotation2d.fromRadians(pigeon.getRoll().getValue().in(Units.Radians));
-        inputs.AccelerationX = pigeon.getAccelerationX().getValue().in(Units.MetersPerSecondPerSecond);
-        inputs.AccelerationY = pigeon.getAccelerationY().getValue().in(Units.MetersPerSecondPerSecond);
-        inputs.AccelerationZ = pigeon.getAccelerationZ().getValue().in(Units.MetersPerSecondPerSecond);
+//        inputs.AccelerationX = pigeon.getAccelerationX().getValue().in(Units.MetersPerSecondPerSecond);
+//        inputs.AccelerationY = pigeon.getAccelerationY().getValue().in(Units.MetersPerSecondPerSecond);
+//        inputs.AccelerationZ = pigeon.getAccelerationZ().getValue().in(Units.MetersPerSecondPerSecond);
 
-        inputs.odometryYawTimestamps =
-                yawTimestampQueue.stream().mapToDouble((Double value) -> value).toArray();
-        inputs.odometryYawPositions =
-                yawPositionQueue.stream()
-                        .map(x -> Rotation2d.fromDegrees((inverted ? -1 : 1) * x))
-                        .toArray(Rotation2d[]::new);
-        yawTimestampQueue.clear();
-        yawPositionQueue.clear();
+        if (yawTimestampQueue != null) {
+            inputs.odometryYawTimestamps =
+                    yawTimestampQueue.stream().mapToDouble((Double value) -> value).toArray();
+            inputs.odometryYawPositions =
+                    yawPositionQueue.stream()
+                            .map(x -> Rotation2d.fromDegrees((inverted ? -1 : 1) * x))
+                            .toArray(Rotation2d[]::new);
+            yawTimestampQueue.clear();
+            yawPositionQueue.clear();
+        }
     }
 
     @Override
