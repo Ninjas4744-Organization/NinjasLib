c92da1bad8a167304761996bdced34e32fa200e4
eitanVIP <eitangrimblat@gmail.com>
2025-09-16 20:15:36 +0300

    Removed goofy ahh useless maxAcceleration constant from swerve

--- swerve/constants/SwerveConstants.java
+++ swerve/constants/SwerveConstants.java
@@ -21,9 +21,6 @@ public class SwerveConstants {
     /** Max speed the swerve could possibly rotate, the real thing if you want a speed limit do it in the rotationSpeedLimit, in rad/s */
     public double maxAngularVelocity;
 
-    /** Max acceleration the swerve could possibly drive, in m/s^2 */
-    public double maxAcceleration;
-
     /** Max acceleration the swerve could possibly change movement direction (needs to be calibrated), in m/s^2 */
     public double maxSkidAcceleration;
 
