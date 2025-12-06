ae8e60d96adc6651fcf4e06043e35f0a0e4e3b8b
eitanVIP <eitangrimblat@gmail.com>
2025-12-06 22:50:36 +0200

    StateMachine changes - Made end conditions not spam if the transition is illegal. - Switched multi edge's ...parameter to List to avoid confusion about the parameter placement. - Added javadoc

--- statemachine/StateMachineBase.java
+++ statemachine/StateMachineBase.java
@@ -8,6 +8,7 @@ import org.jgrapht.graph.SimpleDirectedGraph;
 import org.littletonrobotics.junction.Logger;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 
@@ -47,7 +48,7 @@ public abstract class StateMachineBase<StateEnum extends Enum<StateEnum>> extend
             Map<Command, StateEnum> ends = stateEnds.get(getCurrentState());
             if(ends != null) {
                 for(Command end : ends.keySet()) {
-                    if(end.isFinished() || !end.isScheduled()) {
+                    if((end.isFinished() || !end.isScheduled()) && canTransitionTo(ends.get(end))) {
                         System.out.println("[StateMachine] Ended state " + getCurrentState().name() + ": " + ends.get(end).name());
                         changeRobotState(ends.get(end), false);
                         break;
@@ -72,6 +73,7 @@ public abstract class StateMachineBase<StateEnum extends Enum<StateEnum>> extend
         }
 
         Logger.recordOutput("StateMachine/Is Transitioning", isTransitioning());
+        Logger.recordOutput("StateMachine/Current State", getCurrentState());
         Logger.recordOutput("StateMachine/Target State", getTargetState() == null ? "N/A" : getTargetState().name());
     }
 
@@ -113,42 +115,97 @@ public abstract class StateMachineBase<StateEnum extends Enum<StateEnum>> extend
         changeRobotState(wantedState, false);
     }
 
+    /**
+     * @param start The start state
+     * @param end The end state
+     * @return Whether there is an edge command connecting the start state to the end state.
+     */
+    public boolean canTransitionTo(StateEnum start, StateEnum end) {
+        return graph.containsEdge(start, end);
+    }
+
+    /**
+     * @param state The state
+     * @return Whether the robot can transition to this state: Whether there is an edge command connecting the current robot state to this state.
+     */
+    public boolean canTransitionTo(StateEnum state) {
+        return graph.containsEdge(getCurrentState(), state);
+    }
+
+    /**
+     * @return Whether the robot is currently transitioning from one state to another. Whether the statemachine is running an edge command.
+     */
     public boolean isTransitioning() {
         return currentEdge != null;
     }
 
+    /**
+     * @return The current edge/transition command.
+     * If the robot is transitioning from one state to another, the edge command will be returned. Otherwise, will return null.
+     */
+    public Command getCurrentTransitionCommand() {
+        return currentEdge;
+    }
+
+    /**
+     * @return The current robot state from RobotStateBase.
+     */
     public StateEnum getCurrentState() {
         return (StateEnum) RobotStateBase.getInstance().getRobotState();
     }
 
+    /**
+     * @return The target state the statemachine is currently transitioning to.
+     * If an edge command is running, the state the edge is transitioning to will be returned. Otherwise, will return null.
+     */
     public StateEnum getTargetState() {
         if(currentEdge != null)
             return graph.getEdgeTarget(currentEdge);
         return null;
     }
 
-    public Command getCurrentTransitionCommand() {
-        return currentEdge;
-    }
-
     /**
      * Set in this function the commands to run in state transitions.
-     * And the state ends suppliers.
+     * And the state end conditions.
      *
      * @see #addEdge(Enum, Enum, Command)
      * @see #addStateEnd(Enum, Map)
      */
     protected abstract void defineGraph();
 
+    /**
+     * Add an edge command connecting the start state to the end state in the statemachine's graph.
+     * @param start The state the command will from.
+     * @param end The state the robot will be when the command finishes.
+     * @param command The transition command (Doesn't need to change the robot state, this is handled by the statemachine).
+     */
     protected void addEdge(StateEnum start, StateEnum end, Command command) {
+        if (start.equals(end)) {
+            System.out.println("[StateMachine] Start and end state of an edge cannot be the same");
+            return;
+        }
+
         graph.addEdge(start, end, command);
     }
 
+    /**
+     * Add a connection from the start state to the end state in the statemachine's graph.
+     * @param start The state to transition from.
+     * @param end The state the robot will be after.
+     * @see #addEdge(Enum, Enum, Command)
+     */
     protected void addEdge(StateEnum start, StateEnum end) {
         addEdge(start, end, Commands.none());
     }
 
-    protected void addMultiEdge(StateEnum end, Supplier<Command> command, StateEnum... start) {
+    /**
+     * Add edges from all start states to the end state.
+     * @param start The list of start states.
+     * @param end The end state.
+     * @param command The edge command.
+     * @see #addEdge(Enum, Enum, Command)
+     */
+    protected void addMultiEdge(List<StateEnum> start, StateEnum end, Supplier<Command> command) {
         for(StateEnum state : start){
             if(state != end) {
                 addEdge(state, end, command.get());
@@ -156,10 +213,23 @@ public abstract class StateMachineBase<StateEnum extends Enum<StateEnum>> extend
         }
     }
 
-    protected void addMultiEdge(StateEnum end, StateEnum... start) {
-        addMultiEdge(end, Commands::none, start);
+    /**
+     * Add edges from all start states to the end state.
+     * @param start The list of start states.
+     * @param end The end state.
+     * @see #addEdge(Enum, Enum, Command)
+     * @see #addEdge(Enum, Enum)
+     */
+    protected void addMultiEdge(List<StateEnum> start, StateEnum end) {
+        addMultiEdge(start, end, Commands::none);
     }
 
+    /**
+     * Add edges from all states to the end state.
+     * @param end The end state.
+     * @param command The edge command.
+     * @see #addEdge(Enum, Enum, Command)
+     */
     protected void addOmniEdge(StateEnum end, Supplier<Command> command) {
         for(StateEnum state : stateEnumClass.getEnumConstants()) {
             if(state != end) {
@@ -168,25 +238,45 @@ public abstract class StateMachineBase<StateEnum extends Enum<StateEnum>> extend
         }
     }
 
-    protected void addCommutativeEdge(StateEnum start, StateEnum end, Supplier<Command> command) {
-        addEdge(start, end, command.get());
-        addEdge(end, start, command.get());
+    /**
+     * Add the same edge for the transition from state1 to state2, and for the transition from state2 to state1.
+     * @param state1 A state.
+     * @param state2 Another state.
+     * @param command The edge command. It is a supplier instead of a command so each edge will get a duplicate of the command to avoid errors.
+     * @see #addEdge(Enum, Enum, Command)
+     */
+    protected void addCommutativeEdge(StateEnum state1, StateEnum state2, Supplier<Command> command) {
+        addEdge(state1, state2, command.get());
+        addEdge(state2, state1, command.get());
     }
 
-    protected void addCommutativeEdge(StateEnum start, StateEnum end) {
-        addEdge(start, end);
-        addEdge(end, start);
+    /**
+     * Add the same edge for the transition from state1 to state2, and for the transition from state2 to state1.
+     * @param state1 A state.
+     * @param state2 Another state.
+     * @see #addEdge(Enum, Enum, Command)
+     * @see #addEdge(Enum, Enum)
+     */
+    protected void addCommutativeEdge(StateEnum state1, StateEnum state2) {
+        addEdge(state1, state2);
+        addEdge(state2, state1);
     }
 
-    protected void addCommutativeEdge(StateEnum start, StateEnum end, Command forward, Command backward) {
-        addEdge(start, end, forward);
-        addEdge(end, start, backward);
-    }
-
+    /**
+     * Add an end condition for a state.
+     * When the command finishes running, the statemachine will fire the transition command between the state to the wanted state.
+     * The commands MUST NOT convey actual logic, but should only be for waiting for some event.
+     * For example, any waitUntil or waitTime commands are accepted.
+     * @param state The state this end condition applies to.
+     * @param nextStatesMap A map of commands to states where each command represents an end condition which when finishes will transition to the matching state.
+     */
     protected void addStateEnd(StateEnum state, Map<Command, StateEnum> nextStatesMap) {
         stateEnds.put(state, nextStatesMap);
     }
 
+    /**
+     * Prints the statemachine graph including states and edges to the console.
+     */
     public void printGraph() {
         System.out.println("---------------StateMachine Graph---------------");
         for (Object vertex : instance.graph.vertexSet()) {
